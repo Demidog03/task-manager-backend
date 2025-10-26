a346d418dfe982d2d65d108e43d6fc7665b3b132
olzhas <olzhasktl362@gmail.com>
2025-10-26 19:45:58 +0500

    add: serverless config

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
-import 'dotenv/config'
+let cachedConnection = (globalThis as any).__mongooseConn as Promise<typeof mongoose> | undefined
 
 export async function connectDB() {
   try {
-    const uri = process.env.MONGODB_URI || ''
-    mongoose.set('strictQuery', true)
-    await mongoose.connect(uri)
-    console.log('Connected to MONGO DB!')
+      if (!cachedConnection) {
+          const uri = process.env.MONGODB_URI || ''
+          mongoose.set('strictQuery', true)
+          cachedConnection = mongoose.connect(uri)
+          console.log('Connected to MONGO DB!')
+      }
+        await cachedConnection
   }
   catch (error) {
     console.log(error)
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import { connectDB } from './db/db.js'
 import { createExpressApp } from './app.js'
 
